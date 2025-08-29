dc37d1386618db4b8c67a8b19afed5bc25251361
Joscha <joscha@plugh.de>
2025-08-29 22:16:50 +0200

    Run commands

---
+++ pasch/cmd.py
@@ -0,0 +1,16 @@
+import shlex
+import subprocess
+
+from rich import print
+from rich.markup import escape
+
+
+def run_check(*cmd: str) -> None:
+    print(f"[bright_black]$ {escape(shlex.join(cmd))}")
+    subprocess.run(cmd, check=True)
+
+
+def run_capture(*cmd: str) -> str:
+    print(f"[bright_black]$ {escape(shlex.join(cmd))}")
+    result = subprocess.run(cmd, check=True, capture_output=True, encoding="utf-8")
+    return result.stdout
