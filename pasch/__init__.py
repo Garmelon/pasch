5bb1105f3859b2456e350e2b6d7d23ed1f4df00b
Joscha <joscha@plugh.de>
2025-11-04 02:39:11 +0100

    Add @module annotation

--- pasch/__init__.py
+++ pasch/__init__.py
@@ -1,10 +1,11 @@
 from . import file, modules, util
-from .orchestrator import Module, Orchestrator
+from .orchestrator import Module, Orchestrator, module
 
 __all__: list[str] = [
     "Module",
     "Orchestrator",
     "file",
+    "module",
     "modules",
     "util",
 ]
--- pasch/orchestrator.py
+++ pasch/orchestrator.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import getpass
 import socket
 from abc import ABC, abstractmethod
+from typing import Callable, Concatenate
 
 from rich import print
 from rich.console import Console
@@ -18,6 +19,36 @@ class Module(ABC):
 
     @abstractmethod
     def realize(self) -> None: ...
+
+
+def _snake_to_camel(s: str) -> str:
+    return "".join(s.capitalize() for s in s.split("_"))
+
+
+# Annotate a function with @module to turn it into a class implementing Module.
+def module[**P](
+    func: Callable[Concatenate[Orchestrator, P], None],
+) -> Callable[Concatenate[Orchestrator, P], None]:
+    def __init__(
+        self,
+        orchestrator: Orchestrator,
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> None:
+        super(self.__class__, self).__init__(orchestrator)
+        self.args = args
+        self.kwargs = kwargs
+
+    def realize(self) -> None:
+        # pyrefly: ignore
+        return func(self.orchestrator, *self.args, **self.kwargs)
+
+    # pyrefly: ignore
+    return type(
+        _snake_to_camel(func.__name__),
+        (Module,),
+        {"__init__": __init__, "realize": realize},
+    )
 
 
 class Orchestrator:
